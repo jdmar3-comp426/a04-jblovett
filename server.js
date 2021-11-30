26c6eb401be9e477fb433eb623e068d203ebdafc
jblovett <jblovett@live.unc.edu>
2021-11-29 20:24:58 -0500

    sql majik

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 // Define app using express
 var express = require("express");
-const md5 = require("md5");
-var app = express()
+var app = express();
 // Require database SCRIPT file
-var db = require("/Users/jblovett/a04-jblovett/database.js")
+var db = require("./database.js");
 // Require md5 MODULE
-var md5_mod = require(md5)
+var md5 = require("md5");
 
 // Make Express use its own built-in body parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set server port
+
+var HTTP_PORT = 5000;
 
 // Start server
 app.listen(HTTP_PORT, () => {
@@ -25,6 +26,10 @@ app.get("/app/", (req, res, next) => {
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
+app.post("/app/users", (req, res) => {
+	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)").run();
+	res.status(200).json(stmt);
+});
 
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {	
@@ -33,13 +38,21 @@ app.get("/app/users", (req, res) => {
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
+app.get("/app/users", (req, res) => {
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = 2").get();
+	res.status(200).json(stmt);
+});
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
+app.patch("/app/users", (req, res) => {
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?").run();
+	res.status(200).json(stmt);
+});
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 
 // Default response for any other request
 app.use(function(req, res){
-	res.json({"message":"Endpoint not found. (404)"});
+	res.json({"message":"Your API is working!"});
     res.status(404);
 });
