d5d312ec4a4984928366575cf6ee4e8145a4b7fd
jblovett <jblovett@live.unc.edu>
2021-11-30 21:20:40 -0500

    follow the patterns

--- server.js
+++ server.js
@@ -69,8 +69,9 @@ app.get("/app/user/:id", (req, res) => {
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
-	const stmt = db.prepare(`UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?`).run(req.body.user, req.body.pass, req.params.id);
-	res.status(200).json(stmt);
+	const stmt = db.prepare(`UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?`);
+	const info = stmt.run(req.body.user, md5(req.body.pass), req.params.id);
+	res.status(200).send({"message": info.changes+ " record updated: ID "+ info.lastInsertRowid+ " (200)"});
 });
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
